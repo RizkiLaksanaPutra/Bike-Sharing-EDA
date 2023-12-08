ae8f30d0314de911b7d87a9447a4573c334977a5
Rizki Laksana Putra <rizkilaksana007@gmail.com>
2023-12-08 18:01:30 +0700

    notebook done, now we work on dashboard

---
+++ dashboard.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
+import streamlit as st
+
